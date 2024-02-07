21a98fe26ea4d63e71ed541883aef2f3cccca67f
Lucika <lucika_jeanbaptiste@yahoo.fr>
2024-02-07 11:25:23 -0500

    first commit

---
+++ backend/schemas/users.js
@@ -0,0 +1,32 @@
+export default {
+    name: 'users',
+    title: 'Users',
+    type: 'document',
+    fields: [
+        {
+            name: 'firstname',
+            title: 'Firstname',
+            type: 'string'
+        },
+        {
+            name: 'lastname',
+            title: 'Lastname',
+            type: 'string'
+        },
+        {
+            name: 'email',
+            title: 'Email',
+            type: 'string'
+        },
+        {
+            name: 'password',
+            title: 'Password',
+            type: 'string'
+        },
+        {
+            name: 'profile_img',
+            title: 'Profile_img',
+            type: 'string'
+        }
+    ]
+}
